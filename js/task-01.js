072382f71752bc705a82afc00fc0407d10ecaa0d
KorotinI <142391798+KorotinI@users.noreply.github.com>
2023-12-15 11:27:41 +0100

    tsk1

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+
+const categories = document.querySelectorAll("#categories .item");
+console.log("Number of categories:", categories.length);
+
+categories.forEach((category) => {
+  const categoryName = category.querySelector("h2");
+  const categoryItems = category.querySelectorAll("ul li");
+
+  console.log(`Category: ${categoryName.textContent}`);
+  console.log(`Elements: ${categoryItems.length}`);
+});
